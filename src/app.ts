a836cc94c92ce8bcfd45a4cdf96e96de315194a6
MD JABED HASAN <138822808+Jabed-Hasan@users.noreply.github.com>
2025-07-02 15:22:06 +0600

    Create Student Interface,Model,route,service,controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/students/student.route';
 const app: Application = express();
 const port = 3000;
 
@@ -8,6 +9,9 @@ app.use(express.json());
 
 // cors
 app.use(cors());
+
+//application routes
+app.use("/api/v1/students", StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
---
+++ src/app/modules/students/student.controller.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student:studentData } = req.body;
+    const result = await StudentServices.createStudentToDB(studentData);
+    res.status(200).json({
+      success: true,
+      message: "Student created successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to create student",
+      error: error,
+    });
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: "Students fetched successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch students",
+      error: error,
+    });
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(id);
+    res.status(200).json({
+      success: true,
+      message: "Student fetched successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch student",
+      error: error,
+    });
+  }
+};
+
+
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};  
+
+
+
+
